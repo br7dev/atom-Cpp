8be7b382e498097ced9898baf0d7726feb6b248d
burhan-reshi <burhanjr7@gmail.com>
2020-06-11 15:05:13 +0530

    typeid andtype.cpp added

---
+++ C++/typeidandtype.cpp
@@ -0,0 +1,14 @@
+#include <any>
+#include <vector>
+#include<bits/stdc++.h>
+
+using namespace std;
+int main(){
+   int sum =0;
+   vector<any> m ;
+   m.push_back(1);
+   m.push_back(2);
+   cout<<typeid(sum).name()<<endl;
+      cout<<typeid(m).name()<<endl;
+
+}
